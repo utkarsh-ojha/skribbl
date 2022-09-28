ed8b36c2947e2c599901050c12652d6399939a14
Abhimanyu Srivastava <Abhimanyu.Srivastava@freschesolutions.com>
2022-09-28 12:13:01 +0530

    changed TreeMap to SortedMap

--- chatserver/src/main/java/com/scribbl/chatserver/services/impl/GameDriverServiceImpl.java
+++ chatserver/src/main/java/com/scribbl/chatserver/services/impl/GameDriverServiceImpl.java
@@ -5,10 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Service
 public class GameDriverServiceImpl implements GameDriverService {
@@ -23,13 +20,13 @@ public class GameDriverServiceImpl implements GameDriverService {
 
         //calling getUsersByRoomId()or api to fetch user details.
         List<String> userList = restTemplate.getForObject(URL_GET_ALL_USER, ArrayList.class);
-        Map<String, Integer> scoreMap = new HashMap<>();
+        SortedMap<String, Integer> scoreMap = new TreeMap<>();
         for (String user : userList) {
             scoreMap.put(user, 0);
         }
         //looping user for each iteration.
-        // one user choose a word and others starts guessing
 
+        // one user choose a word and others starts guessing
         for(String user1:userList){
             // each user gets a chance to choose a particular word
             for(String user2:userList) {
@@ -39,7 +36,7 @@ public class GameDriverServiceImpl implements GameDriverService {
                 }
                 // calculate the score according to successful match of guessed word
                 scoreMap.put(user2,scoreMap.get(user2)+100);
-                // every user will get the score updated like this 
+                // every user will get the score updated like this
             }
         }
 
